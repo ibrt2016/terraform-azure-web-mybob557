73914a8916a2920884f8112330b778b21f322bda
ibrt2016 <ibrt2012@gmail.com>
2024-07-28 23:39:41 -0400

    🔩 Ticket #9890809 - Create Azure resource group and VNet.

---
+++ 9890809-vnet.tf
@@ -0,0 +1,17 @@
+module "azure_virtual_network_9890809" {
+  source  = "claranet/vnet/azurerm"
+  version = "7.0.0"
+
+  environment    = var.environment
+  location       = module.azure_region.location
+  location_short = module.azure_region.location_short
+  client_name    = var.client_name
+  stack          = var.stack
+  name_suffix    = "001"
+
+  resource_group_name = module.rg.resource_group_name
+
+  vnet_cidr   = ["10.26.0.0/16"]
+
+  extra_tags = local.extra_tags
+}
